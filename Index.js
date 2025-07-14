d7303709542e88f6cea59d2b7e0bc6b8952bfacd
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-07-14 15:24:41 +0800

    Delete Index.js

--- Index.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Оюунсанаа чатбот тавтай морил!</h1>
-      <p>Энэ бол таны нүүр хуудас.</p>
-    </div>
-  );
-}
