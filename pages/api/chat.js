6e1d6035402f533700902bda6112f78ea21cbd7c
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-07-14 09:25:06 +0800

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -15,4 +15,6 @@ export default async function handler(req, res) {
 
   const data = await response.json();
   res.status(200).json({ reply: data.choices[0].message.content });
+
+  console.log("triggered redeploy");
 }
